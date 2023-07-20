53971efea7bb776e35de504b5f9dc6355129abf4
krsone21 <59742756+krsone21@users.noreply.github.com>
2023-07-20 10:47:08 -0400

    Working code, added print(end'')

--- Bite 15 enumerate two strings_sequences.py
+++ Bite 15 enumerate two strings_sequences.py
@@ -6,10 +6,11 @@ countries = 'Australia Spain Global Argentina USA Mexico'.split()
 def enumerate_names_countries():
     results = ''
     for index, (name, country) in enumerate(zip(names, countries), 1):
-        results += f'{index}. {name:<10} {country:<11}\n'
-    return results
+        results += f'{index}. {name:<10} {country}\n' #sets second column as width 11, < let justifies it
+    #print(repr(results))  <<used this for debugging
+    print(results, end="")  #print(  end) was needed so that the program did not create a newline, failing the test
 
-print(enumerate_names_countries()) 
+print(enumerate_names_countries())  
 
 
 
