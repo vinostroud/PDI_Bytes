a71d9bcff3f0fbec809ab99017826c974c6faba7
krsone21 <59742756+krsone21@users.noreply.github.com>
2023-08-18 21:20:49 -0400

    updated 43 with a comment

--- Bite43 kwargs keywords.py
+++ Bite43 kwargs keywords.py
@@ -5,4 +5,12 @@ def get_profile(*,name='julian', profession='programmer'):
 
 print(get_profile())
 
-#print(get_profile("bob", "doctor"))
+'''
+This line defines a function named get_profile with the following parameters:
+*: The asterisk * here is used to indicate that all the following parameters must be keyword-only arguments. 
+Keyword-only arguments can only be passed using keyword syntax (e.g., name='Alice'), 
+and they cannot be passed positionally (e.g., 'Alice').
+name: This is a keyword-only parameter with a default value of 'julian'. 
+If no value is provided for name, it will default to 'julian'.
+profession: This is another keyword-only parameter with a default value of 'programmer'. 
+If no value is provided for profession, it will default to 'programmer'.'''
