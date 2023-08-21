4294668101d389102cbbfe39caa973d61780d356
krsone21 <59742756+krsone21@users.noreply.github.com>
2023-08-21 09:50:07 -0400

    working code

--- bite45 usingDequeue_LastFive.py
+++ bite45 usingDequeue_LastFive.py
@@ -5,7 +5,14 @@ def my_queue(n=5):
     queue = collections.deque([], maxlen = n)
     return queue
 
-
+'''
+ a deque object is created using the collections.deque() constructor.
+The deque is initialized with an empty list [] as the initial contents and the specified maximum length n.
+This maximum length means that the deque will only hold a maximum of n elements. 
+If new elements are added when the deque is already at its maximum capacity, the oldest elements 
+will be automatically removed to maintain the maximum length.
+The created deque is returned from the function.
+'''
 
 
 if __name__ == '__main__':
